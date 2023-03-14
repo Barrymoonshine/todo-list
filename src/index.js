268167f499917a96f08c04bbe76e32cf8f907a63
Nick Sheward <nickshwrd@gmail.com>
2023-03-14 14:01:13 +0000

    Image added to test webpack and import functionality working correctly

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-import testScript from "./test.js";
-import "./global-style.css";
+import testScript from './test.js';
+import './global-style.css';
+import listIcon from './assets/images/list.png';
 
 testScript();
+
+const header = document.getElementById('title');
+
+header.innerHTML = `
+<img class="list-icon" src="../src/assets/images/list.png" alt="List icon">`;
