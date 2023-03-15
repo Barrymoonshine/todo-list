0db0613458006f9b5931decd49d91fcecdd39a35
Nick Sheward <nickshwrd@gmail.com>
2023-03-15 09:08:24 +0000

    Add new project modal created

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import './global-style.css';
 import listIcon from './assets/images/list.png';
-import renderDefaultPage from './modules/displayController.js';
+import {
+  renderDefaultPage,
+  displayProjectModal,
+} from './modules/displayController.js';
 
 renderDefaultPage();
+
+const newProjectButton = document.getElementById('add-project');
+
+newProjectButton.addEventListener('click', () => {
+  displayProjectModal();
+  console.log('new project button pressed');
+});
 
 const header = document.getElementById('page-header');
 
--- src/modules/displayController.js
+++ src/modules/displayController.js
@@ -1,5 +1,14 @@
-export default function renderDefaultPage() {
+function renderDefaultPage() {
   const dynamicContainer = document.getElementById('dynamic-container');
   dynamicContainer.innerHTML = `
     Test test one two`;
 }
+
+function displayProjectModal() {
+  const projectModal = document.getElementById('new-project-modal');
+  projectModal.style.display = 'flex';
+  window.style.backgroundColor = 'rgba (0,0,0,0.4)';
+  console.log('display project modal function has been executed');
+}
+
+export { renderDefaultPage, displayProjectModal };
