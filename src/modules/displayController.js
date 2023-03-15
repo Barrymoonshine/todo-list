6bf1c13ce4185a398a686954c9c71d57ab1d8798
Nick Sheward <nickshwrd@gmail.com>
2023-03-15 09:28:29 +0000

    Further updates to tidy up design structure of displayController js file

--- src/modules/displayController.js
+++ src/modules/displayController.js
@@ -1,19 +1,21 @@
-export default function displayController() {
-  function renderDefaultPage() {
+const displayController = (() => {
+  const renderDefaultPage = () => {
     const dynamicContainer = document.getElementById('dynamic-container');
     dynamicContainer.innerHTML = `
       Test test one two`;
-  }
+  };
 
-  function displayProjectModal() {
+  const displayProjectModal = () => {
     const projectModal = document.getElementById('new-project-modal');
     projectModal.style.display = 'flex';
     window.style.backgroundColor = 'rgba (0,0,0,0.4)';
     console.log('display project modal function has been executed');
-  }
+  };
 
   return {
     renderDefaultPage,
     displayProjectModal,
   };
-}
+})();
+
+export default displayController;
